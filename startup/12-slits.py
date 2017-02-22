2556118226674e1926288dadaba95630d365a4d2
Thomas A Caswell <tcaswell@bnl.gov>
2017-02-22 17:29:48 -0500

    add real motors to baffle slits

--- startup/12-slits.py
+++ startup/12-slits.py
@@ -1,11 +1,17 @@
 from ophyd import Device, Component as Cpt, EpicsMotor, PVPositioner, EpicsSignal, EpicsSignalRO
 
-class BaffleSlit(Device):
+class BaffleSlitBase(Device):
     hg = Cpt(EpicsMotor, '-Ax:HG}Mtr')
     hc = Cpt(EpicsMotor, '-Ax:HC}Mtr')
     vg = Cpt(EpicsMotor, '-Ax:VG}Mtr')
     vc = Cpt(EpicsMotor, '-Ax:VC}Mtr')
 
+class BaffleSlit(BaffleSlitBase):    
+    inb = Cpt(EpicsMotor, '-Ax:I}Mtr')
+    out = Cpt(EpicsMotor, '-Ax:O}Mtr')
+    bot = Cpt(EpicsMotor, '-Ax:B}Mtr')
+    top = Cpt(EpicsMotor, '-Ax:T}Mtr')
+    
 
 class VirtualGap(PVPositioner):
     readback = Cpt(EpicsSignalRO, 't2.C')
@@ -42,7 +48,7 @@ m1slt = BaffleSlit('XF:02IDA-OP{Mir:1-Slt:4_D_1', name='m1slt')
 pgmslt_u = BaffleSlit('XF:02IDB-OP{Mono:1-Slt:8_U_1', name='pgmslt_u')
 pgmslt_d = BaffleSlit('XF:02IDB-OP{Mono:1-Slt:9_D_1', name='pgmslt_d')
 
-m3slt = BaffleSlit('XF:02IDC-OP{Mir:3-Slt:12_U_1', name='m3slt')
+m3slt = BaffleSlitBase('XF:02IDC-OP{Mir:3-Slt:12_U_1', name='m3slt')
 m4slt = BaffleSlit('XF:02IDC-OP{Mir:4-Slt:18_U_1', name='m4slt')
 
 extslt = ExitSlit('XF:02IDC-OP{Slt:1-Ax:15', name='extslt')
