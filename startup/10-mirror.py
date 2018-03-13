d485f79c032c6464ec4b75831ca68f49176311d7
SIX Operator <xf02id1@xf02id1-ws2.cs.nsls2.local>
2018-03-13 12:12:35 -0400

    Resolving m5 issues

--- startup/10-mirror.py
+++ startup/10-mirror.py
@@ -1,5 +1,5 @@
-from ophyd import Device, EpicsMotor, EpicsSignalRO,EpicsSignal
-from ophyd import Component as Cpt
+from ophyd import Device, EpicsMotor, EpicsSignalRO, EpicsSignal, PVPositioner
+from ophyd import Component as Cpt, FormattedComponent as FCpt
 
 class M1(Device):
     x = Cpt(EpicsMotor, '_Trans}Mtr')
@@ -18,31 +18,23 @@ class MHexapod(Device):
     pit = Cpt(EpicsMotor, '_Ry}Mtr')
     rol = Cpt(EpicsMotor, '_Rz}Mtr')
 
-class M5_axis(Device):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+class M5_axis(PVPositioner):
+    done = FCpt(EpicsSignalRO, '{self._sliced_prefix}Sts:MoveDone-Sts')
+    def __init__(self, prefix, *args, **kwargs):
+        self._sliced_prefix = ''.join(prefix.partition('}')[:2])
+        super().__init__(prefix, *args, **kwargs)
         self.readback.name = self.name
-
         
-    readback = Cpt(EpicsSignalRO,"-I")
-    setpoint = Cpt(EpicsSignal,"-SP")
-    #I have hard coded the done moving PV here, I will look at options to resolve this.
-    done = Cpt(EpicsSignalRO,"XF:02IDD-ES{Mir:5}Sts:MoveDone-Sts",add_prefix='')
+    readback = Cpt(EpicsSignalRO, "-I")
+    setpoint = Cpt(EpicsSignal, "-SP")
     done_value=1
 
     # Define the class properties here
-    
-    @property
-    def read_val(self):
-        return self.get()
-
     @property
     def hints(self):
         return{'fields':'[self.readback.name]'}
 
-    def set(self,value):
-        self.setpoint.put(value)
-    
+ 
 class EHexapod(Device):
     x = Cpt(M5_axis, 'X')
     y = Cpt(M5_axis, 'Y')
@@ -51,15 +43,12 @@ class EHexapod(Device):
     pit = Cpt(M5_axis, 'Pitch')
     rol = Cpt(M5_axis, 'Roll')
 
-
-    
-
     
     
 m1 = M1('XF:02IDA-OP{Mir:1-Ax:4', name='m1')
 m3 = MHexapod('XF:02IDC-OP{Mir:3-Ax:13', name='m3')
 m4 = MHexapod('XF:02IDC-OP{Mir:4-Ax:18', name='m4')
-m5 = EHexapod('XF:02IDD-ES{Mir:5}Pos:',name='m5')
-m6 = M6('XF:02IDD-ES{Mir:6-Ax:',name='m6')
+m5 = EHexapod('XF:02IDD-ES{Mir:5}Pos:', name='m5')
+m6 = M6('XF:02IDD-ES{Mir:6-Ax:', name='m6')
 
 
