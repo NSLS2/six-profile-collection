7d6609789a57bddcfda65e58ea7f2757a9a8ee20
SIX Operator <xf02id1@xf02id1-ws2.cs.nsls2.local>
2018-03-23 13:59:37 -0400

    updating baseline to include gcdiag and m3diag

--- startup/11-diagnostic.py
+++ startup/11-diagnostic.py
@@ -1,5 +1,6 @@
 from ophyd import EpicsMotor
-
+import collections
+import time
 
 def mv_Diag_SAslit_unit(axis,value):
     '''
@@ -29,18 +30,44 @@ class Diag_SAslit_unit(Device):
     locations : dictionary
         A keyword:Value dictionary that lists all of the predefined locations and axis values 
         for the instance of the class in the form {location1:value1, location2:value2,.....}.
+    in_band : float
+        A float that gives teh in-band range when deciding if the device is 'in' the correct 
+        location or not. THe default value is 0.1.
 
     '''
-    def __init__(self, *args, locations=None, **kwargs):
+    def __init__(self, *args, locations=None,in_band=0.1 , **kwargs):
         super().__init__(*args, **kwargs)
         self.locations = locations
+        self.in_band = in_band
         for location in locations:
-            setattr(self,location,mv_Diag_SAslit_unit(self.y,locations[location]))
+            setattr(self,location,mv_Diag_SAslit_unit(self.y,locations[location]))            
+
             
-                
     y = Cpt(EpicsMotor, '')
 
+    def read(self):
+        '''
+        An attribute that returns the current 'location' of the unit as an ordered dictionary. This
+        is used identically to the read attribute function for a standard device and therefore can 
+        be used in the baseline.
+        
+        Parameters
+        ----------
+        out_dict: ordered dictionary, ouput
+            The output dictionary that matches the standard output for a Device.
 
+        '''
+        loc_value='invalid location'
+        for location in self.locations:
+            if self.y.position >= self.locations[location] - self.in_band and \
+                    self.y.position <= self.locations[location] + self.in_band:
+                loc_value = location
+
+        out_dict = collections.OrderedDict()
+        out_dict[self.name+'_location'] = {'timestamp':time.time(),'value':loc_value }
+                
+        return out_dict
+                
             
 m5mask = Diag_SAslit_unit('XF:02IDD-ES{Msk:Mir5-Ax:Y}Mtr',
                          locations= {'Open':54, 'Thin':34, 'Wide':21, 'Thru':8},
@@ -55,7 +82,7 @@ gcdiag = Diag_SAslit_unit('XF:02IDC-OP{Mir:4-Diag:16_U_1-Ax:1}Mtr',
                          name='gcdiag')
 
 espgmmask = Diag_SAslit_unit('XF:02IDD-ES{Msk:Mono2-Ax:Y}Mtr',
-                         locations= {'YAG':-43.4},
+                         locations= {'YAG':-43.4,'Out':0},
                          name='espgmmask')
 
 
--- startup/96-sd.py
+++ startup/96-sd.py
@@ -3,8 +3,8 @@ epu1.gap, epu1.phase,
 feslt.hc, feslt.vc, feslt.hg, feslt.vg, 
 m1.x, m1.pit, m1.rol, 
 pgm.cff, pgm.en, pgm.gr_x, pgm.m2_pit, pgm.gr_pit, 
-m3slt.hs, m3slt.ha, m3slt.vs, m3slt.va, 
-m3diag.y, 
+m3slt.hs, m3slt.ha, m3slt.vs, m3slt.va,
+m3diag.y,
 m3.x, m3.y, m3.z, m3.yaw, m3.pit, m3.rol,
 extslt.hg, extslt.vg, extslt.hc, 
 gcdiag.y, 
@@ -16,7 +16,7 @@ ow,
 m5.x, m5.y, m5.z, m5.yaw, m5.pit, m5.rol,
 m5mask.y, m6_msk,
 m6.pit, m6.z,
-               espgm.cff, espgm.en, espgm.m7_pit, espgm.gr_pit, espgm.gr_x, espgm.gr_x_rb, espgmmask.y]
+espgm.cff, espgm.en, espgm.m7_pit, espgm.gr_pit, espgm.gr_x, espgm.gr_x_rb, espgmmask.y]
 
 # To avoid baseline are printed on screen:
 # bec.disable_baseline()%
