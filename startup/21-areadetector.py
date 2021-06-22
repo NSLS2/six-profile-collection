b201134465cd39369b52da126c1e10ac18051f07
Maksim Rakitin <mrakitin@bnl.gov>
2021-06-22 17:30:31 -0400

    Do not use stage_sigs for create_directory

--- startup/21-areadetector.py
+++ startup/21-areadetector.py
@@ -16,12 +16,17 @@ start_time=time.monotonic()
 class HDF5PluginWithFileStore(HDF5Plugin_V22, FileStoreHDF5IterativeWrite):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # In CSS help: "N < 0: Up to abs(N) new directory levels will be created"
-        self.stage_sigs.update({"create_directory": -3})
+        self._old_create_directory = ''
 
     def stage(self, *args, **kwargs):
-        self.create_directory.put(self.stage_sigs["create_directory"])
+        self._old_create_directory = self.create_directory.get(as_string=True)
+        # in css help: "n < 0: up to abs(n) new directory levels will be created"
+        self.create_directory.put(-3)
         super().stage(*args, **kwargs)
+
+    def unstage(self, *args, **kwargs):
+        super().unstage(*args, **kwargs)
+        self.create_directory.put(self._old_create_directory)
 
     def get_frames_per_point(self):
         return self.parent.cam.num_images.get()  # HACK fixed from =1 to this self.
